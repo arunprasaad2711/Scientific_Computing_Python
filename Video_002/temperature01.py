6b34598d294fb7c224406bcfb5f1bca9bd2becde
Arun Prasaad Gunasekaran <arunprasaad2711@gmail.com>
2019-08-12 02:51:45 +0530

    Finished Recording Video 002

--- Video_002/temperature01.py
+++ Video_002/temperature01.py
@@ -1,15 +1,15 @@
 """
-Program to ask the name, temperature, and rainfall of a place and print it.
+Program to ask the name, temperature, and rainfall of a place and print it
 """
 
-# This is a variable to save the place
+# This is a variable to save the name of the place
 place = input("Enter the name of the place:")
 
-# Rainfall variable
-rainfall = input("Enter the rainfall recorded today in mm:")
-
-# Temperature variable
+# This is a variable to save the temperature of the place
 temperature = float(input("Enter the temperature recorded today in Celsius:"))
 
+# This is a variable to save the name of the place
+rainfall = float(input("Enter the rainfall recorded today in mm per day:"))
+
 # Print statement
-print("In ", place, " today it rained ", rainfall, " mm  and the temperature is", temperature, " degree celsius")
+print("In", place, "today it rained", rainfall, "mm  and the temperature is", temperature, "degree celsius")
