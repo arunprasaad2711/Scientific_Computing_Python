d110fddbb6e41b4a25a4778e58c3f188be5627a5
Arun Prasaad Gunasekaran <arunprasaad2711@gmail.com>
2019-08-25 02:47:11 +0530

    Finished video 6 for sci-comp python

--- Video_006/Modified_HeronsFormula_backup.py
+++ Video_006/Modified_HeronsFormula_backup.py
@@ -1,5 +1,5 @@
 '''
-Program to calculate the area of a triangle using Heron's formulae
+Program to calculate the area of a triangle using Heron's formula
 '''
 
 # Get inputs
@@ -7,17 +7,21 @@ a = float(input("Enter the side 'a' of the triangle:"))
 b = float(input("Enter the side 'b' of the triangle:"))
 c = float(input("Enter the side 'c' of the triangle:"))
 
-if (a <= 0.0 or b <= 0.0 or c <= 0.0):
-    print("Sides of a triangle can't be zero or negative!")
-elif ( c >= a+b or b >= a+c or a >= b+c):
-    print("The values violate triangle inequality!")
+# Properties of a triangle:
+# Triangle has 3 sides greater than zero.
+# Sum of any two sides is greater than the third side.
+
+if(a <= 0.0 or b <= 0.0 or c <= 0.0):
+	print("Sides of a triangle can't be zero or negative!")
+elif(a >= b+c or b >= a+c or c >= a+b):
+	print("The values violate triangle inequality!")
 else:
-    print("All conditions are valid!")
-    # Calculate the semi-perimeter
-    s = (a + b + c)*0.5
-
-    # Find the Area
-    Area = (s*(s-a)*(s-b)*(s-c))**0.5
-
-    # Print the result
-    print("The area of the triangle with sides a = {}, b = {}, and c = {} is {}".format(a, b, c, Area))
+	print("The input values make a legit triangle!")
+	# Calculate the semi-perimeter
+	s = (a + b + c)*0.5
+	
+	# Find the Area
+	Area = (s*(s-a)*(s-b)*(s-c))**0.5
+	
+	# Print the result
+	print("The area of the triangle with sides a = {}, b = {}, and c = {} is {}".format(a, b, c, Area))
